5646d4c210390802a54c9725f4864929c2b476cf
Sean Corcoran <corcoransean28@gmail.com>
2023-04-21 17:16:43 +0100

    created two tables for rotas and clickable users to filter

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,32 @@
 <template>
-  <v-row class="pa-4">
-    <v-col cols="12" sm="8" md="6">
-      <!-- test rotas -->
+  <v-row>
+    <v-col cols="12" sm="8" md="8">
       <!-- <pre>{{ rotas }}</pre> -->
       <!-- test users -->
-      <pre>{{ users }}</pre>
-      <!-- test single user by id -->
-      <pre>{{ userRota }}</pre>
-      <v-btn elevation="2" @click="fetchRotaById(123)">Fetch User</v-btn>
+      <v-data-table
+        :headers="userHeaders"
+        :items="users"
+        :items-per-page="5"
+        class="elevation-1 mb-4"
+        @click:row="filterRota"
+      ></v-data-table>
+      <!-- test rotas -->
+      <v-data-table
+        :headers="rotaHeaders"
+        :items="filteredRota"
+        :items-per-page="5"
+        class="elevation-1"
+      ></v-data-table>
     </v-col>
-    <v-col cols="12" sm="8" md="6">
-      <v-card class="dashboardn py-4 d-flex">
+    <v-col cols="12" sm="8" md="4">
+      <v-card class="dashboard py-4 d-flex">
         <v-row justify="center">
-          <v-date-picker v-model="picker"></v-date-picker>
+          <v-date-picker></v-date-picker>
+        </v-row>
+      </v-card>
+      <v-card class="pa-4">
+        <v-row>
+          <v-btn elevation="2" @click="fetchAllRotas()">Fetch All Rotas</v-btn>
         </v-row>
       </v-card>
     </v-col>
@@ -24,15 +38,48 @@ import { mapActions, mapGetters } from 'vuex'
 
 export default {
   name: 'Dashboard',
+  data() {
+    return {
+      userHeaders: [
+        {
+          text: 'ID',
+          align: 'start',
+          sortable: false,
+          value: 'id',
+        },
+        { text: 'First Name', value: 'first_name' },
+        { text: 'Last Name', value: 'last_name' },
+        { text: 'Email', value: 'email' },
+      ],
+      rotaHeaders: [
+        {
+          text: 'ID',
+          align: 'start',
+          sortable: false,
+          value: 'id',
+        },
+        { text: 'Type', value: 'type' },
+        { text: 'Date', value: 'date' },
+        { text: 'User ID', value: 'userId' },
+        { text: 'Start Date', value: 'startDate' },
+        { text: 'End Date', value: 'endDate' },
+      ],
+    }
+  },
   computed: {
-    ...mapGetters(['rotas', 'users', 'userRota']),
+    ...mapGetters(['users', 'filteredRota']),
   },
   created() {
-    this.fetchAllRotas()
     this.fetchAllUsers()
+    this.fetchAllRotas()
   },
   methods: {
-    ...mapActions(['fetchAllRotas', 'fetchAllUsers', 'fetchRotaById']),
+    ...mapActions(['fetchAllRotas', 'fetchAllUsers']),
+    filterRota(user) {
+      this.$store.commit('filterRotaById', user.id)
+    },
   },
 }
 </script>
+
+
--- store/index.js
+++ store/index.js
@@ -2,7 +2,7 @@
 export const state = () => ({
   rotas: [],
   users: [],
-  userRota: [],
+  filteredRota: [],
 })
 
 // getters
@@ -13,8 +13,8 @@ export const getters = {
   users(state) {
     return state.users
   },
-  userRota(state) {
-    return state.userRota
+  filteredRota(state) {
+    return state.filteredRota
   },
 }
 
@@ -38,26 +38,37 @@ export const actions = {
       })
       .catch((err) => console.error(err))
   },
-  // users by id
-  fetchRotaById(context, id) {
-    return fetch(`https://my.api.mockaroo.com/rotas/${id}?key=727fabe0`)
-      .then((res) => res.json())
-      .then((data) => {
-        context.commit('setUserRota', data)
-      })
-      .catch((err) => console.error(err))
-  },
+  // user rota by id
+  // fetchRotaById(context, id) {
+  //   return fetch(`https://my.api.mockaroo.com/rotas/${id}?key=727fabe0`)
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       context.commit('setUserRota', data)
+  //     })
+  //     .catch((err) => console.error(err))
+  // },
+  // // filter
+  // filterRotaById(context, id) {
+  //   context.commit('setFilteredRota', this.s)
+  // }
 }
 
 // mutations
 export const mutations = {
   setRotas(state, rotas) {
     state.rotas = rotas
+    state.filteredRota = rotas
   },
   setUsers(state, users) {
-    state.users = users
+    // opt1: filter out duplicate user ids
+    state.users = [...new Map(users.map((user) => [user.id, user])).values()]
+    // opt2: filter out ids that dont exist in rotas
+    // ...
   },
   setUserRota(state, userRota) {
     state.userRota = userRota
+  },
+  filterRotaById(state, id) {
+    state.filteredRota = state.rotas.filter((rota) => rota.userId === id)
   },
 }
